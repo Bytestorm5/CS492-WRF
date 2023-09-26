09d51d85d30fce3ba930d93a475daf652f92a8a4
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2023-09-26 13:40:18 -0400

    BO finished and ready for use?

--- optimize.py
+++ optimize.py
@@ -1,13 +1,14 @@
 import numpy as np
-import mock_controller as Controller
+import wrf_controller as Controller
 from skopt import gp_minimize
 from skopt.utils import use_named_args
 from skopt.space import Real, Integer, Categorical
 
 # ACTUAL PARAMS TBD
-dim_m = Real(low=-5.0, high=5.0, name='m')
-dim_b = Real(low=-5, high=5, name='b')
-dimensions = [dim_m, dim_b]
+dim_qh = Integer(low=450, high=700, name='qh')
+dim_qhl = Integer(low=700, high=900, name='qhl')
+dim_qs = Integer(low=50, high=200, name='qs')
+dimensions = [dim_qh, dim_qhl, dim_qs]
 
 @use_named_args(dimensions=dimensions)
 def evaluate(**params):       
@@ -15,16 +16,18 @@ def evaluate(**params):
     pred = Controller.run_model(params)
 
     # MSE
-    diff = gt - pred
-    eval = (diff ** 2).sum() / len(pred)
-    print(params, eval)
-    return eval
+    SE = 0
+    for pred_t, pred_val in pred.items():
+        SE += (pred_val - gt[pred_t]) ** 2
+    MSE = SE / len(pred.keys())
+    
+    return MSE
 
 def optimize(iterations=10, rng_seed=123) -> tuple[list, float]:
-    result = gp_minimize(func=evaluate, dimensions=dimensions, n_calls=iterations, noise=0.1, random_state=rng_seed)
+    result = gp_minimize(func=evaluate, dimensions=dimensions, n_calls=np.int32(iterations), noise=0.1, random_state=np.int32(rng_seed))
     return result['x'], result['fun']
 
 if __name__ == "__main__":
-    param_result, eval = optimize(iterations=25, rng_seed=635)
+    param_result, eval = optimize(iterations=25, rng_seed=5)
     print("BEST PARAMS:", param_result)
     print("EVAL:", eval)
--- wrf_controller.py
+++ wrf_controller.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import netCDF4
 import pandas as pd
+import subprocess
+import re
 
 # Functions to directly interface with the WRF-Solar Model
 # Abstracting like this makes it easier to test
@@ -9,26 +11,45 @@ import pandas as pd
 # get_true_data() and run_model() should output data in the same format/shape
 
 # Gets the ground truth output data for the model as a numpy array
-def get_true_data() -> tuple[np.ndarray, np.ndarray]:
+def get_true_data() -> dict:
     df = netCDF4.Dataset("sgpradflux10long_area_mean.c2.20090506_1200UTC.nc")
-    return df["obs_time"][:].values, df['obs_swdtot'][:].values
+    return {time: val for time, val in zip(np.array(df["obs_time"][:]), np.array(df['obs_swdtot'][:]))}
 
 # Runs the model with given parameters and returns output as numpy arrays
-def run_model(params) -> tuple[np.ndarray, np.ndarray]:
-    # TODO: Run Model
-    EXECUTABLE_PATH = ""
+def run_model(params: dict) -> dict:
+    # Update namelist with template
+    with open('template.input', 'r') as reader:
+        with open('namelist.input', 'w') as writer:
+            writer.write(reader.read().format(**params))
+    
+    process = subprocess.run("mpiexec -np 9 ./wrf.exe")
+    if process.returncode != 0:
+        print("--- ERROR IN WRF EXECUTION; ABORTING ---")
+        exit()
     
     # Retrive Output Data
-    OUT_DATA_PATH = ""
+    OUT_DATA_PATH = "."
     files = sorted(os.listdir(OUT_DATA_PATH))
     
-    SWDTOTS = np.zeros(len(files))
-    TIMES = np.zeros(len(files))
-    for i, file in enumerate(files):
-        df = netCDF4.Dataset(os.path.join(OUT_DATA_PATH, file))
-        TIMES[i] = df["XTIME"][0]
-        SWDTOTS[i] = df["SWDOWN"][0][:].mean().mean()
-    
-    return TIMES, SWDTOTS
-    
-print(get_true_data())
+    SWDTOTS = []
+    TIMES = []
+    for file in files:
+        path = os.path.join(OUT_DATA_PATH, file)
+        
+        match = re.match(r"wrfout_d01_2009-05-(\d\d)_(\d\d)", file)
+        if match != None:   
+            time = int(match.group(2)) if match.group(1) == '06' else int(match.group(2))+24 
+            print(file, time)  
+            if time < 12 or time > 27:
+                continue
+            
+            # This is stupid but netCDF doesn't play nice with the special characters
+            os.rename(path, path[:-6])
+            df = netCDF4.Dataset(path[:-6])                
+            
+            TIMES.append(time)
+            SWDTOTS.append(df["SWDOWN"][0][:].mean().mean()) 
+            df.close()
+            os.rename(path[:-6], path)    
+        
+    return {time: val for time, val in zip(TIMES, SWDTOTS)}
